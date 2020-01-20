79ae550ae583a4ec20c384d44171fe9711b9fcad
quqp <trhody@quq.jp>
2020-01-20 11:59:22 +0900

    ファイルのディレクトリ位置を修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'sample1/sample1_page.dart';
-import 'sample1/sample2_page.dart';
+import 'sample2/sample2_page.dart';
 
 Future<void> main() async {
   runApp(MaterialApp(
